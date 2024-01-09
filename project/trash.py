170483c12f3cd932acfe5c1cb15b75afb72d7df8
oana-ghiga <oana.r.maria@gmail.com>
2024-01-09 13:33:09 +0200

    trying to fix the score

--- project/trash.py
+++ project/trash.py
@@ -1,64 +1,71 @@
-game_state = [[0 for _ in range(9)] for _ in range(9)]
-previous_states = []
-groups = []
+def calculate_score(game_state):
+    def is_valid(i, j):
+        return 0 <= i < 9 and 0 <= j < 9
 
-class Group:
-    def __init__(self, owner, stones):
-        self.owner = owner
-        self.stones = []
-        for stone in stones:
-            self.stones.append(stone)
+    def dfs(i, j, player):
+        if not is_valid(i, j) or visited[i][j] or game_state[i][j] != 0:
+            return False
+        visited[i][j] = True
+        territory[player].append((i, j))
+        result = True
+        for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
+            result &= dfs(x, y, player)
+        return result
 
-    def get_merged(self, other_group) -> ['Group']:
-        """
-        Merges the current group with another group.
-        Args:
-            other_group: the group to be merged with the current group
-        Returns:
-            a new group containing all the stones from both groups
-        """
-        new_group = Group(self.owner, self.stones)
-        for stone in other_group.stones:
-            new_group.stones.append(stone)
-        return new_group
+    score = [0, 0]
+    visited = [[False] * 9 for _ in range(9)]
+    territory = {1: [], 2: []}
 
-    def compute_liberties(self, game_state) -> int:
-        """
-        Computes the number of liberties of the current group in the given game_state.
-        Args:
-            game_state: the game state to be analyzed
-        Returns:
-            the number of liberties of the current group
-        """
-        liberties = 0
-        for stone in self.stones:
-            row, col = stone
-            adjacent_positions = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
-            for r, c in adjacent_positions:
-                if 0 <= r < len(game_state) and 0 <= c < len(game_state[0]):
-                    if r < 0 or r >= len(game_state) or c < 0 or c >= len(game_state[0]):
-                        continue
-                    if game_state[r][c] == 0:
-                        liberties += 1
-        return liberties
+    for i in range(9):
+        for j in range(9):
+            if game_state[i][j] == 0 and not visited[i][j]:
+                for player in [1, 2]:
+                    if dfs(i, j, player):
+                        if len(territory[player]) > len(territory[3 - player]):
+                            score[player - 1] += len(territory[player])
+                        territory[player] = []
+                        territory[3 - player] = []
+                        break
 
-    def __str__(self):
-        return f"{self.owner}\n{self.stones}"
+    return tuple(score)
+# Example usage:
+game_state_1 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 2, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
 
-def insert_stone(row, col, player):
-    """
-    Inserts a stone in the game state.
-    Args:
-        row: the row where the stone is to be inserted
-        col: the column where the stone is to be inserted
-        player: the player who inserts the stone
-    """
-    game_state[row][col] = player
+game_state_2 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
 
-    new_group = Group(player, [(row, col)])
-    groups.append(new_group)
+game_state_3 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
 
-    adjacent_positions = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
-    for r, c in adjacent_positions
-
-print(groups[0])
+result1 = calculate_score(game_state_1)
+result2 = calculate_score(game_state_2)
+result3 = calculate_score(game_state_3)
+print(result1, result2, result3)
